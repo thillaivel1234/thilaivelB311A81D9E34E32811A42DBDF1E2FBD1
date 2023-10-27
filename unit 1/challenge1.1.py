c934e344c448953e42003a1463ee391a5499a521
thillaivel1234 <149141357+thillaivel1234@users.noreply.github.com>
2023-10-27 11:54:25 +0530

    Add files via upload

---
+++ unit 1/challenge1.1.py
@@ -0,0 +1,12 @@
+def factorial(n):
+ 
+    if n < 1:
+        return 1
+ 
+    return n * factorial(n - 1)
+ 
+ 
+if __name__ == '__main__':
+ 
+    n = 5
+    print(f'The Factorial of {n} is', factorial(n))
